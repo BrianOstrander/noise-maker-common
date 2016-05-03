dd35fbe37df86c8c101a2e0a54772cfc8f70b1f8
Brian Ostrander <Ostrander.Brian@gmail.com>
2016-05-03 01:04:36 -0400

    Added curve node.

---
+++ Assets/Plugins/3rdParty/LunraGames/NoiseMaker/Editor/Editors/CurveNodeEditor.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using System.Linq;
+using UnityEditor;
+using LibNoise;
+using LibNoise.Modifiers;
+using System.Collections.Generic;
+
+
+namespace LunraGames.NoiseMaker
+{
+	[NodeDrawer(typeof(CurveNode), Strings.Modifiers, "Curve")]
+	public class CurveNodeEditor : NodeEditor
+	{
+		public override Node Draw(Graph graph, Node node)
+		{
+			var curve = node as CurveNode;
+
+			if (curve.GetModule(graph.Nodes) != null)
+			{
+				var preview = GetPreview(graph, node);
+				GUILayout.Box(preview.Preview);
+
+				GUILayout.FlexibleSpace();
+
+				var frames = new List<Keyframe>();
+				foreach (var point in curve.Points) frames.Add(new Keyframe((float)point.x, (float)point.y));
+				var animation = new AnimationCurve(frames.ToArray());
+
+				animation = EditorGUILayout.CurveField("Control Points", animation);
+
+				var lastPoints = new List<Vector2>(curve.Points);
+
+
+				curve.Points = new List<Vector2>();
+				foreach (var frame in animation.keys) curve.Points.Add(new Vector2(frame.time, frame.value));
+
+				if (3 < curve.Points.Count)
+				{
+					preview.Stale = preview.Stale || lastPoints.Count != curve.Points.Count;
+
+					if (!preview.Stale)
+					{
+						for (var i = 0; i < lastPoints.Count; i++)
+						{
+							preview.Stale = preview.Stale || lastPoints[i].x != curve.Points[i].x || lastPoints[i].y != curve.Points[i].y;
+						}
+					}
+				}
+				else
+				{
+					curve.Points = lastPoints;
+					preview.Stale = true;
+					UnityEditor.EditorUtility.DisplayDialog("Invalid", "Curve nodes require four or more control points.", "Okay");
+				}
+			}
+			else 
+			{
+				EditorGUILayout.HelpBox(Strings.SpecifyAnInput, MessageType.Warning);
+				GUILayout.FlexibleSpace();
+			}
+
+			return curve;
+		}
+
+	}
+}
---
+++ Assets/Plugins/3rdParty/LunraGames/NoiseMaker/Scripts/Nodes/CurveNode.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using LibNoise;
+using System.Collections.Generic;
+using LibNoise.Modifiers;
+using Atesh;
+
+namespace LunraGames.NoiseMaker
+{
+	public class CurveNode : Node 
+	{
+		/// <summary>
+		/// The points that define the curved output, where X is input/time, Y is output/value.
+		/// </summary>
+		public List<Vector2> Points;
+
+		public override IModule GetModule (List<Node> nodes)
+		{
+			if (SourceIds == null || SourceIds.Count != 1)
+			{
+				if (SourceIds == null) SourceIds = new List<string>();
+				SourceIds.Add(null);
+			}
+			if (StringExtensions.IsNullOrWhiteSpace(SourceIds[0])) return null;
+			var sources = Sources(nodes);
+			if (sources.Count != 1) return null;
+
+			var curve = Module == null ? new CurveOutput(sources[0]) : Module as CurveOutput;
+
+			curve.SourceModule = sources[0];
+
+			if (Points == null)
+			{
+				// newtonsoft seems to have a weird tendancy to *add* to an existing list, instead of replacing it, so we define it here.
+				Points = new List<Vector2> 
+				{
+					new Vector2(-2f, -2f),
+					new Vector2(1f, 1f),
+					new Vector2(-1f, -1f),
+					new Vector2(2f, 2f)
+				};
+			}
+
+			curve.ControlPoints = new List<CurveControlPoint>();
+			foreach (var point in Points) curve.ControlPoints.Add(new CurveControlPoint { Input = point.x, Output = point.y });
+
+			Module = curve;
+
+			return Module;
+		}
+	}
+}
