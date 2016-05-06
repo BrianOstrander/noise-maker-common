ff1bb3b544e809eaa978dd7aa99aa9c38b3b2171
Brian Ostrander <Brian.Ostrander@JumpRampGames.com>
2016-05-06 18:16:37 -0400

    Added min node.

---
+++ Assets/Plugins/3rdParty/LunraGames/NoiseMaker/Editor/Editors/MinNodeEditor.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using UnityEditor;
+using LibNoise;
+
+namespace LunraGames.NoiseMaker
+{
+	[NodeDrawer(typeof(MinNode), Strings.Combiners, "Min")]
+	public class MinNodeEditor : NodeEditor
+	{
+		public override Node Draw(Graph graph, Node node)
+		{
+			var min = node as MinNode;
+
+			if (min.GetModule(graph.Nodes) != null)
+			{
+				var preview = GetPreview(graph, node);
+				GUILayout.Box(preview.Preview);
+			}
+			else EditorGUILayout.HelpBox(Strings.SpecifyTwoInputs, MessageType.Warning);
+
+			return min;
+		}
+	}
+}
---
+++ Assets/Plugins/3rdParty/LunraGames/NoiseMaker/Scripts/Nodes/MinNode.cs
@@ -0,0 +1,32 @@
+﻿using LibNoise;
+using LibNoise.Modifiers;
+using System.Collections.Generic;
+using Atesh;
+
+namespace LunraGames.NoiseMaker
+{
+	public class MinNode : Node
+	{
+		public override IModule GetModule (List<Node> nodes)
+		{
+			if (SourceIds == null || SourceIds.Count != 2)
+			{
+				if (SourceIds == null) SourceIds = new List<string>();
+				SourceIds.Add(null);
+				SourceIds.Add(null);
+			}
+			if (StringExtensions.IsNullOrWhiteSpace(SourceIds[0]) || StringExtensions.IsNullOrWhiteSpace(SourceIds[1])) return null;
+			var sources = Sources(nodes);
+			if (sources.Count != 2) return null;
+
+			var min = Module == null ? new SmallerOutput(sources[0], sources[1]) : Module as SmallerOutput;
+
+			min.SourceModule1 = sources[0];
+			min.SourceModule2 = sources[1];
+
+			Module = min;
+
+			return Module;
+		}
+	}
+}
