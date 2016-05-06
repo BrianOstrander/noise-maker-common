8cab5e9ad12a8f37c95b8043ea7a3aa322869cd7
Brian Ostrander <Brian.Ostrander@JumpRampGames.com>
2016-05-06 17:56:00 -0400

    Added invert node.

---
+++ Assets/Plugins/3rdParty/LunraGames/NoiseMaker/Editor/Editors/InvertNodeEditor.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using UnityEditor;
+using LibNoise;
+
+namespace LunraGames.NoiseMaker
+{
+	[NodeDrawer(typeof(InvertNode), Strings.Modifiers, "Invert")]
+	public class InvertNodeEditer : NodeEditor
+	{
+		public override Node Draw(Graph graph, Node node)
+		{
+			var invert = node as InvertNode;
+
+			if (invert.GetModule(graph.Nodes) != null)
+			{
+				var preview = GetPreview(graph, node);
+				GUILayout.Box(preview.Preview);
+			}
+			else 
+			{
+				EditorGUILayout.HelpBox(Strings.SpecifyAnInput, MessageType.Warning);
+				GUILayout.FlexibleSpace();
+			}
+
+			return invert;
+		}
+	}
+}
---
+++ Assets/Plugins/3rdParty/LunraGames/NoiseMaker/Scripts/Nodes/InvertNode.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using LibNoise;
+using System.Collections.Generic;
+using LibNoise.Modifiers;
+using Atesh;
+
+namespace LunraGames.NoiseMaker
+{
+	public class InvertNode : Node 
+	{
+		public override IModule GetModule (List<Node> nodes)
+		{
+			if (SourceIds == null || SourceIds.Count != 1)
+			{
+				if (SourceIds == null) SourceIds = new List<string>();
+				SourceIds.Add(null);
+			}
+			if (StringExtensions.IsNullOrWhiteSpace(SourceIds[0])) return null;
+			var sources = Sources(nodes);
+			if (sources.Count != 1) return null;
+
+			var invert = Module == null ? new InvertOutput(sources[0]) : Module as InvertOutput;
+
+			invert.SourceModule = sources[0];
+
+			Module = invert;
+
+			return Module;
+		}
+	}
+}
