468b1dbfb156b0eab73a26d8c62aaefd49d718cb
Brian Ostrander <Brian.Ostrander@JumpRampGames.com>
2016-05-06 18:05:53 -0400

    Added max node.

---
+++ Assets/Plugins/3rdParty/LunraGames/NoiseMaker/Editor/Editors/MaxNodeEditor.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using UnityEditor;
+using LibNoise;
+
+namespace LunraGames.NoiseMaker
+{
+	[NodeDrawer(typeof(MaxNode), Strings.Combiners, "Max")]
+	public class MaxNodeEditor : NodeEditor
+	{
+		public override Node Draw(Graph graph, Node node)
+		{
+			var max = node as MaxNode;
+
+			if (max.GetModule(graph.Nodes) != null)
+			{
+				var preview = GetPreview(graph, node);
+				GUILayout.Box(preview.Preview);
+			}
+			else EditorGUILayout.HelpBox(Strings.SpecifyTwoInputs, MessageType.Warning);
+
+			return max;
+		}
+	}
+}
---
+++ Assets/Plugins/3rdParty/LunraGames/NoiseMaker/Scripts/Nodes/MaxNode.cs
@@ -0,0 +1,32 @@
+﻿using LibNoise;
+using LibNoise.Modifiers;
+using System.Collections.Generic;
+using Atesh;
+
+namespace LunraGames.NoiseMaker
+{
+	public class MaxNode : Node
+	{
+		public override IModule GetModule (List<Node> nodes)
+		{
+			if (SourceIds == null || SourceIds.Count != 2)
+			{
+				if (SourceIds == null) SourceIds = new List<string>();
+				SourceIds.Add(null);
+				SourceIds.Add(null);
+			}
+			if (StringExtensions.IsNullOrWhiteSpace(SourceIds[0]) || StringExtensions.IsNullOrWhiteSpace(SourceIds[1])) return null;
+			var sources = Sources(nodes);
+			if (sources.Count != 2) return null;
+
+			var max = Module == null ? new LargerOutput(sources[0], sources[1]) : Module as LargerOutput;
+
+			max.SourceModule1 = sources[0];
+			max.SourceModule2 = sources[1];
+
+			Module = max;
+
+			return Module;
+		}
+	}
+}
