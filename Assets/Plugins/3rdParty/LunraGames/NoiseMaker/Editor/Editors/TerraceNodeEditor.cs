4ecfe9c7cd8524beb7b1bf7bed270dd16129ba22
Brian Ostrander <Ostrander.Brian@gmail.com>
2016-05-08 11:36:55 -0400

    Added terrace node.

---
+++ Assets/Plugins/3rdParty/LunraGames/NoiseMaker/Editor/Editors/TerraceNodeEditor.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using System.Linq;
+using UnityEditor;
+using LibNoise;
+using LibNoise.Modifiers;
+using System.Collections.Generic;
+
+
+namespace LunraGames.NoiseMaker
+{
+	[NodeDrawer(typeof(TerraceNode), Strings.Modifiers, "Terrace")]
+	public class TerraceNodeEditor : NodeEditor
+	{
+		static Dictionary<string, Vector2> ScrollPositions = new Dictionary<string, Vector2>();
+
+		public override Node Draw(Graph graph, Node node)
+		{
+			var terrace = node as TerraceNode;
+
+			if (terrace.GetModule(graph.Nodes) != null)
+			{
+				var preview = GetPreview(graph, node);
+				GUILayout.Box(preview.Preview);
+
+				GUILayout.FlexibleSpace();
+
+				var lastPoints = new List<float>(terrace.Points);
+
+				var scrollPos = Vector2.zero;
+				if (ScrollPositions.ContainsKey(terrace.Id)) scrollPos = ScrollPositions[terrace.Id];
+				else ScrollPositions.Add(terrace.Id, scrollPos);
+
+				if (GUILayout.Button("Add")) terrace.Points.Add(0f);
+
+				scrollPos = GUILayout.BeginScrollView(new Vector2(0f, scrollPos.y), false, false, GUILayout.Height(96f));
+				{
+					var showDelete = 2 < terrace.Points.Count;
+					int? deletedIndex = null;
+					for (var i = 0; i < terrace.Points.Count; i++)
+					{
+						var unmodifiedI = i;
+						GUILayout.BeginHorizontal();
+						terrace.Points[i] = EditorGUILayout.FloatField(terrace.Points[unmodifiedI]);
+						if (showDelete && GUILayout.Button("X")) deletedIndex = unmodifiedI;
+						GUILayout.EndHorizontal();
+					}
+
+					if (deletedIndex.HasValue) terrace.Points.RemoveAt(deletedIndex.Value);
+
+					GUILayout.FlexibleSpace();
+				}
+				GUILayout.EndScrollView();
+
+				ScrollPositions[terrace.Id] = scrollPos;
+
+				preview.Stale = preview.Stale || lastPoints.Count != terrace.Points.Count;
+
+				if (!preview.Stale)
+				{
+					for (var i = 0; i < lastPoints.Count; i++)
+					{
+						preview.Stale = preview.Stale || lastPoints[i] != terrace.Points[i];
+					}
+				}
+			}
+			else 
+			{
+				EditorGUILayout.HelpBox(Strings.SpecifyAnInput, MessageType.Warning);
+				GUILayout.FlexibleSpace();
+			}
+
+			return terrace;
+		}
+
+	}
+}
---
+++ Assets/Plugins/3rdParty/LunraGames/NoiseMaker/Scripts/Nodes/TerraceNode.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using LibNoise;
+using System.Collections.Generic;
+using LibNoise.Modifiers;
+using Atesh;
+
+namespace LunraGames.NoiseMaker
+{
+	public class TerraceNode : Node 
+	{
+		/// <summary>
+		/// The points that define the terraced output, where X is input/time, Y is output/value.
+		/// </summary>
+		public List<float> Points;
+
+		public override IModule GetModule (List<Node> nodes)
+		{
+			if (SourceIds == null || SourceIds.Count != 1)
+			{
+				if (SourceIds == null) SourceIds = new List<string>();
+				SourceIds.Add(null);
+			}
+			if (StringExtensions.IsNullOrWhiteSpace(SourceIds[0])) return null;
+			var sources = Sources(nodes);
+			if (sources.Count != 1) return null;
+
+			var terrace = Module == null ? new Terrace(sources[0]) : Module as Terrace;
+
+			terrace.SourceModule = sources[0];
+
+			if (Points == null)
+			{
+				// newtonsoft seems to have a weird tendancy to *add* to an existing list, instead of replacing it, so we define it here.
+				Points = new List<float> { -1f, 1f };
+			}
+
+			terrace.ControlPoints = new List<double>();
+			foreach (var point in Points) terrace.ControlPoints.Add(point);
+
+			Module = terrace;
+
+			return Module;
+		}
+	}
+}
