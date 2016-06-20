6c22523658503a5fcf873559241b09ccff378873
Brian Ostrander <Ostrander.Brian@gmail.com>
2016-06-20 13:13:29 -0400

    Added Vector3Node.

---
+++ Assets/Plugins/3rdParty/LunraGames/NoiseMaker/Editor/Scripts/NodeEditors/Vector3NodeEditor.cs
@@ -0,0 +1,20 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace LunraGames.NoiseMaker
+{
+	[NodeDrawer(typeof(Vector3Node), Strings.Properties, "Vector3")]
+	public class Vector3NodeEditor : NodeEditor
+	{
+		public override INode Draw(Graph graph, INode node)
+		{
+			var vector3Node = node as Vector3Node;
+
+			var preview = GetPreview<Vector3>(graph, node);
+
+			vector3Node.Value = Deltas.DetectDelta<Vector3>(vector3Node.Value, EditorGUILayout.Vector3Field("Value", vector3Node.Value), ref preview.Stale);
+
+			return vector3Node;
+		}
+	}
+}
---
+++ Assets/Plugins/3rdParty/LunraGames/NoiseMaker/Scripts/Nodes/Vector3Node.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+namespace LunraGames.NoiseMaker
+{
+	public class Vector3Node : Node<Vector3>
+	{
+		public override Vector3 GetValue (List<INode> nodes)
+		{
+			return Value;
+		}
+	}
+}
