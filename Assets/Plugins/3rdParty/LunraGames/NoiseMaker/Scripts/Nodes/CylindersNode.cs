929aa05c245cabb340ca458c25174674816ea1de
Brian Ostrander <Ostrander.Brian@gmail.com>
2016-06-20 13:15:35 -0400

    Converted Cylinders to new paradigm.

--- Assets/Plugins/3rdParty/LunraGames/NoiseMaker/Scripts/Nodes/CylindersNode.cs
+++ Assets/Plugins/3rdParty/LunraGames/NoiseMaker/Scripts/Nodes/CylindersNode.cs
@@ -8,13 +8,14 @@ namespace LunraGames.NoiseMaker
 {
 	public class CylindersNode : Node<IModule>
 	{
+		[NodeLinker(0)]
 		public float Frequency = 0.02f;
 
 		public override IModule GetValue (List<INode> nodes)
 		{
 			var cylinders = Value == null ? new Cylinders() : Value as Cylinders;
 
-			cylinders.Frequency = Frequency;
+			cylinders.Frequency = GetLocalIfValueNull<float>(Frequency, 0, nodes);
 
 			Value = cylinders;
 			return Value;
