df5d760b907b10d240d433db76b2b2ddc12c5596
Brian Ostrander <Ostrander.Brian@gmail.com>
2016-06-26 12:46:56 -0400

    Added Integer Range Node.

--- Assets/Plugins/3rdParty/LunraGames/NoiseMaker/Editor/Scripts/NodeEditors/IntegerRangeNodeEditor.cs
+++ Assets/Plugins/3rdParty/LunraGames/NoiseMaker/Editor/Scripts/NodeEditors/IntegerRangeNodeEditor.cs
@@ -4,16 +4,16 @@ using UnityEditor;
 
 namespace LunraGames.NoiseMaker
 {
-	[NodeDrawer(typeof(FloatRangeNode), Strings.Utility, "Float Range")]
-	public class FloatRangeNodeEditor : NodeEditor 
+	[NodeDrawer(typeof(IntegerRangeNode), Strings.Utility, "Integer Range")]
+	public class IntegerRangeNodeEditor : NodeEditor 
 	{
 		public override INode Draw(Graph graph, INode node)
 		{
-			var rangeNode = node as FloatRangeNode;
+			var rangeNode = node as IntegerRangeNode;
 
 			if (rangeNode.UpperBound < rangeNode.LowerBound) EditorGUILayout.HelpBox("Upper bound cannot be less than lower bound.", MessageType.Warning);
 
-			rangeNode = DrawFields(graph, rangeNode, false) as FloatRangeNode;
+			rangeNode = DrawFields(graph, rangeNode, false) as IntegerRangeNode;
 			var currValue = rangeNode.GetValue(graph);
 
 			GUILayout.BeginHorizontal();
--- Assets/Plugins/3rdParty/LunraGames/NoiseMaker/Scripts/Nodes/IntegerRangeNode.cs
+++ Assets/Plugins/3rdParty/LunraGames/NoiseMaker/Scripts/Nodes/IntegerRangeNode.cs
@@ -3,23 +3,23 @@ using UnityEngine;
 
 namespace LunraGames.NoiseMaker
 {
-	public class FloatRangeNode : Node<float> 
+	public class IntegerRangeNode : Node<int> 
 	{
 		[NodeLinker(0)]
-		public float LowerBound = -1f;
+		public int LowerBound = -1;
 		[NodeLinker(1)]
-		public float UpperBound = 1f;
+		public int UpperBound = 1;
 		[NodeLinker(2)]
 		public int Seed = DemonUtility.IntSeed;
 		[NodeLinker(3)]
 		public RangeOverrides RangeOverride;
 
-		public override float GetValue (Graph graph)
+		public override int GetValue (Graph graph)
 		{
 			var values = NullableValues(graph);
 
-			var lowerBound = GetLocalIfValueNull<float>(LowerBound, 0, values);
-			var upperBound = GetLocalIfValueNull<float>(UpperBound, 1, values);
+			var lowerBound = GetLocalIfValueNull<int>(LowerBound, 0, values);
+			var upperBound = GetLocalIfValueNull<int>(UpperBound, 1, values);
 			var seed = GetLocalIfValueNull<int>(Seed, 2, values);
 			var rangeOverride = GetLocalIfValueNull<RangeOverrides>(RangeOverride, 3, values);
 
@@ -31,13 +31,13 @@ namespace LunraGames.NoiseMaker
 			if (upperBound < lowerBound) 
 			{
 				if (!Application.isEditor || (Application.isEditor && Application.isPlaying)) Debug.LogError("UpperBound must be greater than LowerBound");
-				return 0f;
+				return 0;
 			}
 
 			if (rangeOverride == RangeOverrides.Average)
 			{
-				if (Mathf.Approximately(lowerBound, upperBound)) return lowerBound;
-				else return lowerBound + ((upperBound - lowerBound) * 0.5f);
+				if (lowerBound == upperBound) return lowerBound;
+				else return lowerBound + ((upperBound - lowerBound) / 2);
 			}
 
 			Random.seed = seed;
