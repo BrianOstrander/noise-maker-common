c19af5e50c2f2150aa6298aa9a93880624da3c81
Brian Ostrander <ostrander.brian@gmail.com>
2016-09-21 00:50:59 -0400

    Consolidated noisemaker to use singletonne.

--- Assets/Plugins/3rdParty/LunraGames/NoiseMaker/Editor/Scripts/NoiseMakerConfig.cs
+++ Assets/Plugins/3rdParty/LunraGames/NoiseMaker/Editor/Scripts/NoiseMakerConfig.cs
@@ -1,26 +1,12 @@
 ﻿using UnityEngine;
 using UnityEditor;
+using LunraGames.Singletonnes;
+using System;
 
 namespace LunraGames.NoiseMaker
 {
-	public class NoiseMakerConfig : ScriptableObject 
+	public class NoiseMakerConfig : EditorScriptableSingleton<NoiseMakerConfig>
 	{
-		static NoiseMakerConfig _Instance;
-
-		public static NoiseMakerConfig Instance { get { return _Instance ?? ( _Instance = FindInstance()); } }
-
-		static NoiseMakerConfig FindInstance()
-		{
-			var instances = AssetDatabase.FindAssets("LunraGamesNoiseMakerSettings");
-			if (instances.Length != 1) 
-			{
-				Debug.LogError(instances.Length == 0 ? "No instance of Noise Maker settings exist" : "More than one instance of Noise Maker settings exists");
-				return null;
-			}
-			return AssetDatabase.LoadAssetAtPath<NoiseMakerConfig>(AssetDatabase.GUIDToAssetPath(instances[0]));
-
-		}
-
 		public Texture2D[] LoadingPinwheels;
 
 		#region Elevation preview resources
